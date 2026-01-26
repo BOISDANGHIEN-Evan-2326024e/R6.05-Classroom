4478d5015f7fc85ed31f39c7839359c84c82c9da
Foucher Mathias <mathias.foucher@etu.univ-amu.fr>
2026-01-26 11:56:46 +0100

    premier push

---
+++ front-end/classroom-front/src/pages/Dashboard.tsx
@@ -0,0 +1,8 @@
+export default function Dashboard() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-8">
+      <h1 className="text-3xl font-bold">Dashboard</h1>
+      <p className="text-gray-600 mt-2">Bienvenue sur votre tableau de bord</p>
+    </div>
+  )
+}
