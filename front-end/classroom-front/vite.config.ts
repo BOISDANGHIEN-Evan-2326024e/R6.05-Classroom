491625eecc2d14b3649541cc5c56e1d4ca51ef58
Foucher Mathias <mathias.foucher@etu.univ-amu.fr>
2026-01-26 12:12:43 +0100

    fix front

--- front-end/classroom-front/vite.config.ts
+++ front-end/classroom-front/vite.config.ts
@@ -6,6 +6,10 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true,
+    // Ajoute ce middleware pour le fallback SPA :
+    fs: {
+      allow: ['.'],
+    }
   },
   build: {
     outDir: 'dist',
