95e28c55cb09cd693f4df5ea9c6265230445fed5
Evan <evan.boisdanghien@etu.univ-amu.fr>
2026-02-02 09:50:33 +0100

    videos fix

--- back-end/classroom_back/src/Controller/SecurityController.php
+++ back-end/classroom_back/src/Controller/SecurityController.php
@@ -12,14 +12,11 @@ class SecurityController extends AbstractController
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // Si l'utilisateur est déjà connecté, on le redirige
         if ($this->getUser()) {
-            return $this->redirectToRoute('app_home'); // Change 'app_home' par ta route d'accueil
+            return $this->redirectToRoute('teacher_dashboard');
         }
 
-        // Récupérer l'erreur de connexion s'il y en a une
         $error = $authenticationUtils->getLastAuthenticationError();
-        // Récupérer le dernier nom d'utilisateur (email) saisi par l'internaute
         $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('security/login.html.twig', [
@@ -31,7 +28,6 @@ class SecurityController extends AbstractController
     #[Route(path: '/logout', name: 'app_logout')]
     public function logout(): void
     {
-        // Cette méthode peut rester vide, elle sera interceptée par le firewall
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
